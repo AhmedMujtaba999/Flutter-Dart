eab63e56e1844f0ccdfa6dbe9fe1aeffd3efdfe0
AhmedMujtaba999 <116835811+AhmedMujtaba999@users.noreply.github.com>
2025-12-21 22:44:39 +0530

    List in dart with one data type (specific data type: string, int , bool...)

---
+++ list2.dart
@@ -0,0 +1,11 @@
+void main() {
+List<String> names = ['marry','john', 'tina']; //write datatype in <>
+  names.add('ahmed');
+  names.remove('tina');
+ // names.add(30); it will be an error
+// can have different data types
+  print(names);
+  
+}
+
+
